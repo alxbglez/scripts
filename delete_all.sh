d1f85076fc428b2ed7f7cb6fd1a967c1b453a7b2
Alejandro Blasio González <ablasio@bestday.com>
2020-06-04 14:14:46 -0500

    se agrega el namespace

--- delete_all.sh
+++ delete_all.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
-SERVICE=$1
+NAMESPACE=$1
+SERVICE=$2
 
 if [ ! -n "$SERVICE" ]
 then
 	echo "$0 - Error \$SERVICE not set or NULL"
 else
-  echo Eliminar todo el servicio de $SERVICE
-  echo "Deployments"
-  kubectl get deployments --namespace shared-fraudprevention $SERVICE
-  echo "Services"
-  kubectl get services --namespace shared-fraudprevention $SERVICE
-  echo "Ingress"
-  kubectl get ingress --namespace shared-fraudprevention $SERVICE
+    if [ ! -n "$NAMESPACEE" ]
+    then
+        echo "$0 - Error \$NAMESPACE not set or NULL"
+    else
+    echo Eliminar todo el servicio de $SERVICE - $NAMESPACE
+    echo "Deployments"
+    kubectl get deployments --namespace shared-$NAMESPACE $SERVICE
+    echo "Services"
+    kubectl get services --namespace shared-$NAMESPACE $SERVICE
+    echo "Ingress"
+    kubectl get ingress --namespace shared-$NAMESPACE $SERVICE
 
-  echo "Eliminando Deployments"
-  kubectl delete deployment --namespace shared-fraudprevention $SERVICE
-  echo "Eliminando Services"
-  kubectl delete service --namespace shared-fraudprevention $SERVICE
-  echo "Eliminando Ingress"
-  kubectl delete ingress --namespace shared-fraudprevention $SERVICE
+    echo "Eliminando Deployments"
+    kubectl delete deployment --namespace shared-$NAMESPACE $SERVICE
+    echo "Eliminando Services"
+    kubectl delete service --namespace shared-$NAMESPACE $SERVICE
+    echo "Eliminando Ingress"
+    kubectl delete ingress --namespace shared-$NAMESPACE $SERVICE
+    fi
 fi
-
-
-
