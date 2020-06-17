2aa986409ab4632fc714b3832e760b202259d49a
Alejandro Blasio González <ablasio@bestday.com>
2020-06-16 19:50:15 -0500

    .

--- create_mysql.sh
+++ create_mysql.sh
@@ -5,5 +5,5 @@ docker volume prune
 docker volume create frd-mysql-db-data
 docker volume ls
 docker run -d -p 33060:3306 --name frd-mysql-db  -e MYSQL_ROOT_PASSWORD=sysfraud --mount src=frd-mysql-db-data,dst=/var/lib/mysql mysql
-docker exec -it frd-mysql-db mysql -p
+docker exec -it frd-mysql-db mysql -p sysfraud
 exit
