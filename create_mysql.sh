70a95f24ecde92ccd6843a0366295bb059cb48d9
Alejandro Blasio González <ablasio@bestday.com>
2020-06-16 20:15:03 -0500

    .

--- create_mysql.sh
+++ create_mysql.sh
@@ -5,24 +5,15 @@ docker volume prune
 docker volume create frd-mysql-db-data
 docker volume ls
 docker run -d -p 33060:3306 --name frd-mysql-db  -e MYSQL_ROOT_PASSWORD=sysfraud --mount src=frd-mysql-db-data,dst=/var/lib/mysql mysql
-echo 'Waiting 1m for creation'
+echo 'Waiting for creation'
 
 x=30
 while [ $x -gt 0 ]
 do
-  echo -n "$x\r"
+  echo '$x'
   sleep 1s
   x=$(( $x - 1 ))
 done
+echo 'go'
 
 docker exec -i frd-mysql-db mysql -u root --password=sysfraud
-
-x=5
-while [ $x -gt 0 ]
-do
-  echo -n "$x\r"
-  sleep 1s
-  x=$(( $x - 1 ))
-done
-
-exit
