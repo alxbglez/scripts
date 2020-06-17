9f719b34d88d07a66d8cf6cfd18e707a91ea6b3b
Alejandro Blasio González <ablasio@bestday.com>
2020-06-16 20:02:08 -0500

    .

--- create_mysql.sh
+++ create_mysql.sh
@@ -6,6 +6,19 @@ docker volume create frd-mysql-db-data
 docker volume ls
 docker run -d -p 33060:3306 --name frd-mysql-db  -e MYSQL_ROOT_PASSWORD=sysfraud --mount src=frd-mysql-db-data,dst=/var/lib/mysql mysql
 echo 'Waiting 1m for creation'
-sleep 1m
+
+$i = 0;
+while :
+do
+  if $i == 60
+  then
+    echo "Continue"
+    break
+  fi
+  $i = $i + 1;
+  echo -ne "$i sec."\\r
+  sleep 1s
+done
+
 docker exec -i frd-mysql-db mysql -u root --password=sysfraud
 exit
