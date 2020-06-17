459e4c2b9d86a85977ec817efa3c9ffdae1f4798
Alejandro Blasio González <ablasio@bestday.com>
2020-06-16 19:37:08 -0500

    add mysql

---
+++ create_mysql.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+docker run -d -p 33060:3306 --name frd-mysql-db -e MYSQL_ROOT_PASSWORD=sysfraud mysql
+docker ps -a
+docker exec -it frd-mysql-db mysql -p password
+exit
+docker rm -f frd-mysql-db
+docker volume prune
+docker volume create frd-mysql-db-data
+docker volume ls
+docker run -d -p 33060:3306 --name frd-mysql-db  -e MYSQL_ROOT_PASSWORD=sysfraud --mount src=frd-mysql-db-data,dst=/var/lib/mysql mysql
+docker exec -it frd-mysql-db mysql -p
+exit
+docker rm -f frd-mysql-db
+docker run -d -p 33060:3306 --name frd-mysql-db  -e MYSQL_ROOT_PASSWORD=sysfraud --mount src=frd-mysql-db-data,dst=/var/lib/mysql mysql
+docker exec -it frd-mysql-db mysql -p
